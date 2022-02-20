ed5ecd6da193cb5962016a7c4f5153e0a694eaf9
Pierre Lemaigre <pierrelemaigre.pro@gmail.com>
2022-02-20 15:31:58 +0100

    C

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:projet_transverse_equipe_7/view/consultant/webApp.dart';
+import 'package:projet_transverse_equipe_7/view/patient/HomeScreenPage.dart';
 import 'package:projet_transverse_equipe_7/view/patient/IntroductionAppScreen.dart';
 import 'package:projet_transverse_equipe_7/view/patient/PrescriptionMainPage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -53,6 +54,6 @@ class _HomePage extends State<HomePage> {
 
   @override
   Widget build(BuildContext context){
-    return kIsWeb? WebApp() : intro_screens ? IntroductionAppScreen() : HomePage();
+    return kIsWeb? WebApp() : !intro_screens ? IntroductionAppScreen() : HomeScreenPage();
   }
 }
